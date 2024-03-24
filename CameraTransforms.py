ea69f424919a4350a95a858fda68c831b980cb7e
Luke Tang <tanlu824@student.otago.ac.nz>
2024-03-25 12:58:04 +1300

    Add animation showing camera transformations

---
+++ CameraTransforms.py
@@ -0,0 +1,39 @@
+from manim import *
+from manim_rt import Camera
+
+
+class CameraTransforms(ThreeDScene):
+    def construct(self):
+        self.set_camera_orientation(phi = 45 * DEGREES, theta = -85 * DEGREES, gamma = 95 * DEGREES, zoom = 1)
+        
+        camera = Camera.drawCamera(focal_point_coords=[0.5, 0, 3])
+        
+        y_axis_translation_text = Text("Translation along Y axis")
+        y_axis_translation_text.to_edge(DOWN)
+        
+        x_axis_text = Text("Rotation along X axis")
+        x_axis_text.to_edge(DOWN)
+        y_axis_text = Text("Rotation along Y axis")
+        y_axis_text.to_edge(DOWN)
+        z_axis_text = Text("Rotation along Z axis")
+        z_axis_text.to_edge(DOWN)
+        
+        self.add(camera)
+        self.add_fixed_in_frame_mobjects(y_axis_translation_text)
+        
+        # Translation along Y axis
+        self.play(camera.animate.shift(4 * UP))
+        self.play(camera.animate.shift(7 * DOWN))
+        self.play(camera.animate.shift(3 * UP))
+        
+        # Rotation along X axis
+        self.play(Transform(y_axis_translation_text, x_axis_text))
+        self.play(Rotate(camera, 360 * DEGREES, RIGHT, run_time=3))
+        
+        # Rotation along Y axis
+        self.play(Transform(y_axis_translation_text, y_axis_text))
+        self.play(Rotate(camera, 360 * DEGREES, UP, run_time=3))
+        
+        # Rotation along Z axis
+        self.play(Transform(y_axis_translation_text, z_axis_text))
+        self.play(Rotate(camera, 360 * DEGREES, OUT, run_time=3))
--- manim_rt/Camera.py
+++ manim_rt/Camera.py
@@ -1,7 +1,11 @@
 from manim import *
 
 
-def drawCamera(focal_point_coords = [0, 0, 5], number_plane_grid = False):
+def drawCamera(focal_point_coords = [0, 0, 5], number_plane_grid = True):
+        top_x_coord = 2.5 + focal_point_coords[0]
+        bottom_x_coord = -2.5 + focal_point_coords[0]
+        left_y_coord = 2.5 + focal_point_coords[1]
+        right_y_coord = -2.5 + focal_point_coords[1]
         plane_z_coord = focal_point_coords[2]
         
         focal_point = Dot3D(focal_point_coords, radius = 0.15, color = WHITE)
@@ -13,17 +17,17 @@ def drawCamera(focal_point_coords = [0, 0, 5], number_plane_grid = False):
             grid.move_to([focal_point_coords[0], focal_point_coords[1], focal_point_coords[2] - 5])
         else:
             # Grid made up of 4 3D lines
-            top = Line3D([2.5, 2.5, plane_z_coord - 5], [2.5, -2.5, plane_z_coord - 5], color=WHITE)
-            bottom = Line3D([-2.5, 2.5, plane_z_coord - 5], [-2.5, -2.5, plane_z_coord - 5], color=WHITE)
-            left = Line3D([2.5, 2.5, plane_z_coord - 5], [-2.5, 2.5, plane_z_coord - 5], color=WHITE)
-            right = Line3D([2.5, -2.5, plane_z_coord - 5], [-2.5, -2.5, plane_z_coord - 5], color=WHITE)
+            top = Line3D([top_x_coord, left_y_coord, plane_z_coord - 5], [top_x_coord, right_y_coord, plane_z_coord - 5], color=WHITE)
+            bottom = Line3D([bottom_x_coord, left_y_coord, plane_z_coord - 5], [bottom_x_coord, right_y_coord, plane_z_coord - 5], color=WHITE)
+            left = Line3D([top_x_coord, left_y_coord, plane_z_coord - 5], [bottom_x_coord, left_y_coord, plane_z_coord - 5], color=WHITE)
+            right = Line3D([top_x_coord, right_y_coord, plane_z_coord - 5], [bottom_x_coord, right_y_coord, plane_z_coord - 5], color=WHITE)
             grid = Group(top, bottom, left, right) 
         
         # Frustum made up of 4 3D lines
-        top_left = Line3D(focal_point_coords, [2.5, 2.5, plane_z_coord - 5], color=WHITE)
-        bottom_left = Line3D(focal_point_coords, [-2.5, 2.5, plane_z_coord - 5], color=WHITE)
-        top_right = Line3D(focal_point_coords, [2.5, -2.5, plane_z_coord - 5], color=WHITE)
-        bottom_right = Line3D(focal_point_coords, [-2.5, -2.5, plane_z_coord - 5], color=WHITE)
+        top_left = Line3D(focal_point_coords, [top_x_coord, left_y_coord, plane_z_coord - 5], color=WHITE)
+        bottom_left = Line3D(focal_point_coords, [bottom_x_coord, left_y_coord, plane_z_coord - 5], color=WHITE)
+        top_right = Line3D(focal_point_coords, [top_x_coord, right_y_coord, plane_z_coord - 5], color=WHITE)
+        bottom_right = Line3D(focal_point_coords, [bottom_x_coord, right_y_coord, plane_z_coord - 5], color=WHITE)
         
         camera = Group(focal_point, top_left, top_right, bottom_left, bottom_right, grid)
         
