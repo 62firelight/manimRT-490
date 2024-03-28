0aea649c6ce7b4771dd481e4440b3ee32be2d368
Luke Tang <tanlu824@student.otago.ac.nz>
2024-03-29 12:23:32 +1300

    Ray object with parameterized distance

--- RayObjectIntersectionsPoC.py
+++ RayObjectIntersectionsPoC.py
@@ -5,7 +5,7 @@ from manim_rt import Camera
 class RayObjectIntersectionsPoC(ThreeDScene):
     def construct(self):
         # self.set_camera_orientation(phi = -190 * DEGREES, theta = -85 * DEGREES, gamma = 95 * DEGREES, zoom = 0.75)
-        self.set_camera_orientation(phi = 45 * DEGREES, zoom = 1)
+        self.set_camera_orientation(phi = 75 * DEGREES, theta = -85 * DEGREES, gamma = 95 * DEGREES, zoom = 1)
         
         projection_point_coords = [0, 0, 2.5]
         
@@ -13,30 +13,9 @@ class RayObjectIntersectionsPoC(ThreeDScene):
         camera = camera_obj.get_camera()
         self.add(camera)
         
-        ray = camera_obj.draw_ray(4, 6)
+        ray_obj = camera_obj.draw_ray(8, 5, 1)
+        ray = ray_obj.get_mobject()
         self.add(ray)
-        
-        # ray1 = Arrow3D(projection_point_coords, camera_obj.get_grid().c2p(0.5, -1.5), color=RED)
-        # self.add(ray1)
-        
-        # # lambda = 2
-        # sphere = Sphere(radius=1, resolution=(16, 16)).shift([0.9, 0.3, -5])
-        
-        # # # ray direction: [0.45, 0.15, -5]
-        # # ray1_start_end_coords = [0.45, 0.15, 0]
-        # # ray1_start = Line3D(projection_point_coords, ray1_start_end_coords, color = RED)
-        
-        # # # lambda = 3
-        # ray1_end_end_coords = [1.35, 0.45, -10]
-        # # ray1_end = Arrow3D(ray1_start_end_coords, ray1_end_end_coords, color = RED)
-        
-        # # ray1 = Group(ray1_start, ray1_end)
-        # ray1 = Arrow3D(projection_point_coords, ray1_end_end_coords, color=RED)
-        
-        # self.begin_ambient_camera_rotation(0)
-        
-        # self.add(ray1, sphere)
-        # # self.play(Create(ray1))
-        # # self.play(Create(sphere))
-        
-        
+        ray_obj.set_color(RED)
+        new_ray = ray_obj.change_distance(2)
+        self.add(new_ray)
--- manim_rt/Camera.py
+++ manim_rt/Camera.py
@@ -1,18 +1,6 @@
 from manim import *
+import numpy as np
 
-
-def clamp(n, min, max): 
-    """
-    Quick helper function for limiting numbers.
-    
-    source: https://www.geeksforgeeks.org/how-to-clamp-floating-numbers-in-python/ 
-    """
-    if n < min: 
-        return min
-    elif n > max: 
-        return max
-    else: 
-        return n 
     
 class Camera:
     def __init__(self, projection_point_coords=[0, 0, 0], focal_length=1, width=16, height=9, total_width=5, total_height=5):
@@ -46,13 +34,17 @@ class Camera:
         
         self.camera = Group(self.projection_point, self.top_left, self.top_right, self.bottom_left, self.bottom_right, self.grid)
         
-    def draw_ray(self, x, y):
-        pixel_x_coord = clamp(x, 1, self.width)
-        pixel_y_coord = -clamp(y, 1, self.height)
+    def draw_ray(self, x, y, distance_steps=2, color=BLUE):
+        pixel_x_coord = round(x)
+        pixel_y_coord = round(y)
+        
+        pixel_x_coord = clamp(pixel_x_coord, 1, self.width)
+        pixel_y_coord = -clamp(pixel_y_coord, 1, self.height)
         
         pixel_coords = self.grid.c2p(pixel_x_coord - 0.5, pixel_y_coord + 0.5)
         
-        ray = Line3D(self.projection_point_coords, pixel_coords, color=RED)
+        # TODO: creating another object inside this object method -- possibly bad design??
+        ray = Ray(self.projection_point_coords, pixel_coords, distance_steps)
         
         return ray
         
@@ -76,36 +68,90 @@ class Camera:
     
     def get_grid(self):
         return self.grid
+    
+class Ray:
+    def __init__(self, start_point, end_point, distance_steps=2, color=BLUE):
+        self.start_point = start_point
+        self.direction = np.subtract(end_point, start_point)
+        self.distance_steps = distance_steps
+        self.color = color
         
+        mobject_end_point = self.start_point + self.distance_steps * np.array(self.direction)
+        
+        self.mobject = Arrow3D(start_point, mobject_end_point, color=color)
+    
+    def get_start_point(self):
+        return self.start_point
+    
+    def get_direction(self):
+        return self.direction
+    
+    def get_distance_steps(self):
+        return self.distance_steps
+    
+    def get_mobject(self):
+        return self.mobject
+    
+    def get_color(self):
+        return self.color
+    
+    def set_color(self, color):
+        self.color = color
+    
+    def change_distance(self, distance_steps):
+        self.distance_steps = distance_steps
+        
+        mobject_end_point = self.start_point + self.distance_steps * np.array(self.direction)
+        
+        self.mobject = Arrow3D(self.start_point, mobject_end_point, color=self.color)
+        
+        return self.mobject
+    
+def clamp(n, min, max): 
+    """
+    Quick helper function for limiting numbers.
+    
+    source: https://www.geeksforgeeks.org/how-to-clamp-floating-numbers-in-python/ 
+    """
+    if n < min: 
+        return min
+    elif n > max: 
+        return max
+    else: 
+        return n 
 
 def drawCamera(projection_point_coords = [0, 0, 0], focal_length=1, number_plane_grid = True):
-        top_x_coord = 2.5 + projection_point_coords[0]
-        bottom_x_coord = -2.5 + projection_point_coords[0]
-        left_y_coord = 2.5 + projection_point_coords[1]
-        right_y_coord = -2.5 + projection_point_coords[1]
-        plane_z_coord = projection_point_coords[2]
-        
-        projection_point = Dot3D(projection_point_coords, radius = 0.15, color = WHITE)
-        
-        if number_plane_grid:
-            # Grid made up of a number plane
-            # TODO: parameterize for grid width and height (i.e., camera resolution)
-            grid = NumberPlane(x_range = [-1, 1, 1/3], y_range = [-1, 1, 1/3], x_length = 5, y_length = 5)
-            grid.move_to([projection_point_coords[0], projection_point_coords[1], projection_point_coords[2] - focal_length])
-        else:
-            # Grid made up of 4 3D lines
-            top = Line3D([top_x_coord, left_y_coord, plane_z_coord - focal_length], [top_x_coord, right_y_coord, plane_z_coord - focal_length], color=WHITE)
-            bottom = Line3D([bottom_x_coord, left_y_coord, plane_z_coord - focal_length], [bottom_x_coord, right_y_coord, plane_z_coord - focal_length], color=WHITE)
-            left = Line3D([top_x_coord, left_y_coord, plane_z_coord - focal_length], [bottom_x_coord, left_y_coord, plane_z_coord - focal_length], color=WHITE)
-            right = Line3D([top_x_coord, right_y_coord, plane_z_coord - focal_length], [bottom_x_coord, right_y_coord, plane_z_coord - focal_length], color=WHITE)
-            grid = Group(top, bottom, left, right) 
-        
-        # Frustum made up of 4 3D lines
-        top_left = Line3D(projection_point_coords, [top_x_coord, left_y_coord, plane_z_coord - focal_length], color=WHITE)
-        bottom_left = Line3D(projection_point_coords, [bottom_x_coord, left_y_coord, plane_z_coord - focal_length], color=WHITE)
-        top_right = Line3D(projection_point_coords, [top_x_coord, right_y_coord, plane_z_coord - focal_length], color=WHITE)
-        bottom_right = Line3D(projection_point_coords, [bottom_x_coord, right_y_coord, plane_z_coord - focal_length], color=WHITE)
-        
-        camera = Group(projection_point, top_left, top_right, bottom_left, bottom_right, grid)
-        
-        return camera
+    """
+    Legacy function. Use Camera objects to create a ManimRT Camera.
+    """
+
+    top_x_coord = 2.5 + projection_point_coords[0]
+    bottom_x_coord = -2.5 + projection_point_coords[0]
+    left_y_coord = 2.5 + projection_point_coords[1]
+    right_y_coord = -2.5 + projection_point_coords[1]
+    plane_z_coord = projection_point_coords[2]
+    
+    projection_point = Dot3D(projection_point_coords, radius = 0.15, color = WHITE)
+    
+    if number_plane_grid:
+        # Grid made up of a number plane
+        # TODO: parameterize for grid width and height (i.e., camera resolution)
+        grid = NumberPlane(x_range = [-1, 1, 1/3], y_range = [-1, 1, 1/3], x_length = 5, y_length = 5)
+        grid.move_to([projection_point_coords[0], projection_point_coords[1], projection_point_coords[2] - focal_length])
+    else:
+        # Grid made up of 4 3D lines
+        top = Line3D([top_x_coord, left_y_coord, plane_z_coord - focal_length], [top_x_coord, right_y_coord, plane_z_coord - focal_length], color=WHITE)
+        bottom = Line3D([bottom_x_coord, left_y_coord, plane_z_coord - focal_length], [bottom_x_coord, right_y_coord, plane_z_coord - focal_length], color=WHITE)
+        left = Line3D([top_x_coord, left_y_coord, plane_z_coord - focal_length], [bottom_x_coord, left_y_coord, plane_z_coord - focal_length], color=WHITE)
+        right = Line3D([top_x_coord, right_y_coord, plane_z_coord - focal_length], [bottom_x_coord, right_y_coord, plane_z_coord - focal_length], color=WHITE)
+        grid = Group(top, bottom, left, right) 
+    
+    # Frustum made up of 4 3D lines
+    top_left = Line3D(projection_point_coords, [top_x_coord, left_y_coord, plane_z_coord - focal_length], color=WHITE)
+    bottom_left = Line3D(projection_point_coords, [bottom_x_coord, left_y_coord, plane_z_coord - focal_length], color=WHITE)
+    top_right = Line3D(projection_point_coords, [top_x_coord, right_y_coord, plane_z_coord - focal_length], color=WHITE)
+    bottom_right = Line3D(projection_point_coords, [bottom_x_coord, right_y_coord, plane_z_coord - focal_length], color=WHITE)
+    
+    camera = Group(projection_point, top_left, top_right, bottom_left, bottom_right, grid)
+    
+    return camera
