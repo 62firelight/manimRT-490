Fix calculation of object transformation

manim_rt/RTSphere.py
@@ -6,21 +6,32 @@ from typing import Sequence
 class RTSphere(Sphere):
     def __init__(
         self,
-        center: Point3D = ORIGIN,
+        translation: Sequence[float] = [0, 0, 0],
         x_scale: float = 1,
         y_scale: float = 1,
         z_scale: float = 1,
         x_rotation: float = 0,
         y_rotation: float = 0,
         z_rotation: float = 0,
+        center: Point3D = ORIGIN,
         resolution: Sequence[int] | None = None,
         u_range: Sequence[float] = (0, TAU),
         v_range: Sequence[float] = (0, PI),
         **kwargs,
     ) -> None:
         # get 3x1 translation vector
-        self.translation = np.array([center[0], center[1], center[2]])
-        self.inverse_translation = np.negative(self.translation)
+        self.translation = np.array([
+            [1, 0, 0, translation[0]],  
+            [0, 1, 0, translation[1]],
+            [0, 0, 1, translation[2]],
+            [0, 0, 0, 1],
+        ])
+        self.inverse_translation = np.array([
+            [1, 0, 0, -translation[0]],  
+            [0, 1, 0, -translation[1]],
+            [0, 0, 1, -translation[2]],
+            [0, 0, 0, 1],
+        ])
         
         # get 3x3 scale matrix
         self.scale_matrix = np.array([
@@ -30,29 +41,33 @@ class RTSphere(Sphere):
         ])
         
         # get 3x3 rotation matrix
-        self.rot_matrix = np.zeros((3, 3))
-        if x_rotation != 0:
-            self.rot_matrix += rotation_matrix(x_rotation, RIGHT)
-        if y_rotation != 0:
-            self.rot_matrix += rotation_matrix(y_rotation, UP)
-        if z_rotation != 0:
-            self.rot_matrix += rotation_matrix(z_rotation, OUT)
+        self.rot_matrix = np.identity(3)
+        eps = np.finfo(float).eps
+        # if abs(x_rotation) < eps:
+        #     self.rot_matrix = np.matmul(rotation_matrix(x_rotation, RIGHT), self.rot_matrix)
+        # if abs(y_rotation) < eps:
+        #     self.rot_matrix = np.matmul(rotation_matrix(y_rotation, UP), self.rot_matrix)
+        # if abs(z_rotation) < eps:
+        #     self.rot_matrix = np.matmul(rotation_matrix(z_rotation, OUT), self.rot_matrix)
+        
+        self.rot_matrix = np.matmul(rotation_matrix(x_rotation, RIGHT), self.rot_matrix)
+        self.rot_matrix = np.matmul(rotation_matrix(y_rotation, UP), self.rot_matrix)
+        self.rot_matrix = np.matmul(rotation_matrix(z_rotation, OUT), self.rot_matrix)
         
         # combine scale and rotation matrices and compute inverse
-        self.transform = self.scale_matrix + self.rot_matrix
-        self.inverse_scale_rot_matrix = np.linalg.inv(self.transform)
+        self.transform = np.matmul(self.rot_matrix, self.scale_matrix)
         
         # combine scale and rotation matrices with translation vector
-        self.transform = np.c_[self.transform, self.translation]
+        self.transform = np.c_[self.transform, np.array([0, 0, 0])]
         
         # ensure the linear transformation is stored as homogeneous coordinates
         self.transform = np.r_[self.transform, np.array([[0, 0, 0, 1]])]
         
-        # combine inverse scale and inverse rotation matrices with inverse translation vector
-        self.inverse = np.c_[self.inverse_scale_rot_matrix, self.inverse_translation]
+        # multiply translation matrix with transformation matrix
+        self.transform = np.matmul(self.translation, self.transform)
         
-        # ensure the inverse transformation is stored as homogeneous coordinates
-        self.inverse = np.r_[self.inverse, np.array([[0, 0, 0, 1]])]
+        # calculate inverse
+        self.inverse = np.linalg.inv(self.transform)
         
         self.unit_form = Sphere()
 
@@ -76,6 +91,5 @@ class RTSphere(Sphere):
         self.rotate(y_rotation, UP)
         self.rotate(z_rotation, OUT)
         
-        print(self.transform)
-        print(self.inverse)
+        self.shift(translation)
         

manim_rt/Ray3D.py
@@ -32,9 +32,9 @@ class Ray3D(Arrow3D):
         # TODO: Round any floating point numbers 
         # TODO: Hide .0 for float numbers that are whole for consistency
         if not homogenous_coordinates:
-            equation = "{} + \lambda {}".format(np.ndarray.tolist(self.start), np.ndarray.tolist(self.direction))
+            equation = "{} + \\lambda {}".format(np.ndarray.tolist(self.start), np.ndarray.tolist(self.direction))
         else:
-            equation = "{} + \lambda {}".format(np.ndarray.tolist(self.homogeneous_start), np.ndarray.tolist(self.homogeneous_direction))
+            equation = "{} + \\lambda {}".format(np.ndarray.tolist(self.homogeneous_start), np.ndarray.tolist(self.homogeneous_direction))
         
         return equation
         
@@ -81,8 +81,6 @@ class Ray3D(Arrow3D):
             start_inverse = np.matmul(object.inverse, self.homogeneous_start)
             direction_inverse = np.matmul(object.inverse, self.homogeneous_direction)
             
-            print(start_inverse, direction_inverse)
-            
             inhomogeneous_start_inverse = start_inverse[:3]
             inhomogeneous_direction_inverse = direction_inverse[:3]
             
@@ -91,8 +89,6 @@ class Ray3D(Arrow3D):
             c = np.dot(inhomogeneous_start_inverse, inhomogeneous_start_inverse) - 1
             
             hit_locations = self.quadratic_formula(a, b, c)
-            
-            print(hit_locations)
             
             hit_points = []
             normals = []
@@ -111,8 +107,6 @@ class Ray3D(Arrow3D):
         
             self.hit_points = hit_points
             self.normals = normals
-            
-            print(hit_points)
             
             return hit_points
         else: