bb4412276e300e0c845806f32f188f33b162de6b
62firelight <lktfirelight@gmail.com>
2024-04-26 14:25:24 +1200

    Add intersection dots

--- RTPointLightSourceTest.py
+++ RTPointLightSourceTest.py
@@ -23,19 +23,25 @@ class RTPointLightSourceTest(ThreeDScene):
         
         hit_points = ray.get_intersection(sphere)
         
-        unit_normal = Ray3D(hit_points[0], ray.get_unit_normal(0), color=BLUE)
+        first_hit_point = hit_points[0]
+        first_hit_point_dot = Dot3D(first_hit_point, color=PURPLE)
+        
+        second_hit_point = hit_points[1]
+        second_hit_point_dot = Dot3D(second_hit_point, color=PURPLE)
+        
+        unit_normal = Ray3D(first_hit_point, ray.get_unit_normal(0), color=BLUE)
         unit_normal_text = MathTex("\\hat{n}").next_to(unit_normal.get_end(), RIGHT + OUT, buff=0.1)
         
-        light_vector = Ray3D(hit_points[0], ray.get_light_vector(0, light), color=YELLOW)
+        light_vector = Ray3D(first_hit_point, ray.get_light_vector(0, light), color=YELLOW)
         light_vector_text = MathTex("\\hat{l}").next_to(light_vector.get_end(), RIGHT, buff=0.2)
         
-        reflected_light_vector = Ray3D(hit_points[0], ray.get_reflected_light_vector(0, light), color=ORANGE)
+        reflected_light_vector = Ray3D(first_hit_point, ray.get_reflected_light_vector(0, light), color=ORANGE)
         reflected_light_vector_text = MathTex("\\hat{r}").next_to(reflected_light_vector.get_end(), RIGHT + OUT, buff=0.1)
         
         camera = RTCamera([-3, 0, 3], focal_length=1, plane_width=3, plane_height=3, total_width=1, total_height=1)
         camera.rotate(-45 * DEGREES, UP, camera.projection_point_coords)
         
-        viewer_vector = Ray3D(hit_points[0], ray.get_viewer_vector(0, camera), color=GREEN)
+        viewer_vector = Ray3D(first_hit_point, ray.get_viewer_vector(0, camera), color=GREEN)
         viewer_vector_text = MathTex("\\hat{v}").next_to(viewer_vector.get_end(), IN)
         
         shadow_ray = ray.get_shadow_ray(0, light, color=LIGHT_BROWN)
@@ -45,5 +51,5 @@ class RTPointLightSourceTest(ThreeDScene):
         blocking_sphere = RTSphere([2, 0, 2])
         blocking_sphere.set_color(LIGHT_GRAY)
         
-        self.add(axes, sphere, light, camera, ray, unit_normal, light_vector, reflected_light_vector, viewer_vector, shadow_ray)
+        self.add(axes, sphere, light, camera, ray, first_hit_point_dot, second_hit_point_dot, unit_normal, light_vector, reflected_light_vector, viewer_vector, shadow_ray)
         self.add_fixed_orientation_mobjects(x_label, z_label, ray_text, unit_normal_text, light_vector_text, reflected_light_vector_text, viewer_vector_text, shadow_ray_text)
