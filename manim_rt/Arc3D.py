85298d1f0ae02c0942331429f15472fc3930b66e
62firelight <lktfirelight@gmail.com>
2024-05-02 10:50:36 +1200

    Simplify Arc3D by removing Dot3D mobjects

--- manim_rt/Arc3D.py
+++ manim_rt/Arc3D.py
@@ -1,9 +1,10 @@
 from manim import *
 
 # Source: aman00__ and uwezi in the Manim discord
+# This code was modified so that it was closer to the center of the lines
 class Arc3D(ArcBetweenPoints):
-    def __init__(self, line1, line2, color=WHITE):    
-        dot1 = Dot3D().move_to((line1.get_end() - line1.get_start()) / 2)          
-        dot2 = Dot3D().move_to((line2.get_end() - line2.get_start()) / 2)
+    def __init__(self, line1, line2, color=WHITE):            
+        line1_center = (line1.get_end() - line1.get_start()) / 2
+        line2_center = (line2.get_end() - line2.get_start()) / 2
         
-        super().__init__(dot1.get_center(), dot2.get_center(), radius=1, color=color)
+        super().__init__(line1_center, line2_center, radius=1, color=color)
