a924d919119c5091d89d99a2f766ffe0c5099bdb
62firelight <lktfirelight@gmail.com>
2024-07-18 17:29:34 +1200

    Produce image with eye for report

--- Eye3DTest.py
+++ Eye3DTest.py
@@ -1,19 +1,30 @@
 from manim import *
 
+from manim_rt.RTPlane import RTPlane
+from manim_rt.RTPointLightSource import RTPointLightSource
 from manim_rt.Ray3D import Ray3D
 
 class Eye3DTest(ThreeDScene):
     def construct(self):
-        self.set_camera_orientation(phi=45 * DEGREES, theta=45 * DEGREES)
+        self.set_camera_orientation(phi=45 * DEGREES, theta=-90 * DEGREES)
+        # self.camera.background_color = DARKER_GREY
         
         axes = ThreeDAxes()
         labels = axes.get_axis_labels()
         
-        eye = ImageMobject("Eye.png")
+        eye = ImageMobject("Eye.png").rotate(-45 * DEGREES).shift([-4, 0, 1])
         
-        ray = Ray3D([5, 0, 0], LEFT, distance=4.1, color=YELLOW)
+        light = RTPointLightSource([4, 0, 1])
+    
+        ray1 = Line3D([4, 0, 1], [0, 0, -3], color=YELLOW)
+        ray2 = Ray3D([0, 0, -3], [-1, 0, 1], distance=3.5, color=YELLOW)
         
-        # self.add(axes, labels, eye, ray)
+        miss1 = Line3D([3.5, 0.25, 1], [0, 0.25, -3], color=YELLOW)
+        miss2 = Ray3D([0, 0.25, -3], [-1, 0, 1.5], distance=3.5, color=YELLOW)
         
-        self.play(GrowFromCenter(eye))
-        self.play(Create(ray))
+        plane = RTPlane([0, 0, -3], x_scale=5, y_scale=1.5, color=BLUE, opacity=0.75)
+        
+        self.add(eye, light, ray1, ray2, miss1, miss2, plane)
+        
+        # self.play(GrowFromCenter(eye))
+        # self.play(Create(ray))
