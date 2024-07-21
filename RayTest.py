5711478d76c11bcb3bf3b283f6729d2b63675436
Luke Tang <tanlu824@student.otago.ac.nz>
2024-07-21 21:21:49 +1200

    Modify RayTest for report

--- RayTest.py
+++ RayTest.py
@@ -4,8 +4,32 @@ from manim_rt.Ray3D import Ray3D
 
 class RayTest(ThreeDScene):
     def construct(self):
-        self.set_camera_orientation(phi=25 * DEGREES)
+        self.set_camera_orientation(phi=45 * DEGREES, theta=-45 * DEGREES, zoom=4)
         
-        ray = Ray3D(ORIGIN, [0, 1, 0], 3, color=BLUE)
+        axes = ThreeDAxes()
+        labels = axes.get_axis_labels()
         
-        self.add(ray)
+        x_ray = Ray3D(ORIGIN, [1, 0, 0], 1, color=RED)
+        y_ray = Ray3D(ORIGIN, [0, 1, 0], 1, color=BLUE)
+        z_ray = Ray3D(ORIGIN, [0, 0, 1], 1, color=GREEN)
+        
+        x_text = Tex("X", color=RED).next_to(x_ray.get_end())
+        y_text = Tex("Y", color=BLUE).next_to(y_ray.get_end(), buff=0.03)
+        z_text = Tex("Z", color=GREEN).next_to(z_ray.get_end(), buff=0.05)
+        
+        x_ray_equation = MathTex(x_ray.get_equation(), color=RED)
+        y_ray_equation = MathTex(y_ray.get_equation(), color=BLUE)
+        z_ray_equation = MathTex(z_ray.get_equation(), color=GREEN)
+        
+        equations = VGroup(x_ray_equation, y_ray_equation, z_ray_equation).arrange(DOWN).move_to([-0.25, -0.75, 0])
+        
+        # self.add(axes)
+        # self.add(labels)
+        self.add(x_ray)
+        self.add(y_ray)
+        self.add(z_ray)
+        
+        self.add_fixed_orientation_mobjects(equations)
+        self.add_fixed_orientation_mobjects(x_text)
+        self.add_fixed_orientation_mobjects(y_text)
+        self.add_fixed_orientation_mobjects(z_text)
